20deb8c59d2e124867f75a6cb675466851ac069e
Ciloe <gabiciloe@gmail.com>
2024-03-13 16:29:43 -0300

    Inserindo o value do Qual serie está?
    Inserindo o value do Qual serie está?

--- calculadora-de-gastos-escolar/script.js
+++ calculadora-de-gastos-escolar/script.js
@@ -1,10 +1,19 @@
-const serieOptions = document.getElementById('serieOptions').options;
-const serieOptionsAttr = document.getElementById('serieOptions').attributes;
-const serieOptionsAttrElement = document.getElementById('serieOptions').options;
 
 
-console.log(serieOptions);
-console.log(serieOptionsAttr);
-console.log(serieOptionsAttrElement);
+
+let select = document.getElementById('serieOptions');
+let valorMensalidade = document.getElementById('valorMensalidade').value;
 
 
+let qualSerie;
+let valorMes;
+select.onchange = function(){
+    qualSerie = this.value;
+    let alunoSerie = parseInt(qualSerie)
+    console.log(alunoSerie);
+    
+    console.log( typeof alunoSerie);
+
+    let soma = alunoSerie*valorMes;
+    console.log(soma)
+}
