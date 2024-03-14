0c9693a3ff90e47ef45166bdef6406f466c91dbc
Ciloe <gabiciloe@gmail.com>
2024-03-14 09:00:19 -0300

    Tentando pegar os botoes.value
    Executar uma função para somar eles e atribuir a soma a uma variavel

--- calculadora-de-gastos-escolar/script.js
+++ calculadora-de-gastos-escolar/script.js
@@ -1,12 +1,14 @@
 function calcularGastos(){
     
     let select = document.getElementById('serieOptions');
-    let qualSerie;
     let valorMensalidade = document.getElementById('valorMensalidade');
-
-    console.log( typeof valorMensalidade)
-    let valorMensal = parseInt(valorMensalidade.value);
     
+    let materialIncluso = document.getElementById('materialIncluso');
+    let materialNaoIncluso = document.getElementById('materialNaoIncluso');
+    let qualSerie;
+
+    console.log( typeof valorMensalidade);
+    let valorMensal = parseInt(valorMensalidade.value);
     
     qualSerie = select.value;
     let alunoSerie = parseInt(qualSerie)
@@ -14,8 +16,21 @@ function calcularGastos(){
     
     console.log( typeof alunoSerie);
 
+
+    /**
+     * Aqui vai ser o valor da mensalidade * (a quantidade de anos * 12) = soma
+     */
     let soma = (alunoSerie * 12) * valorMensal;
     alert(soma);
+
+
+    materialIncluso = function(){
+        let materialInclusoRes = alunoSerie * 12;
+
+        alert (materialInclusoRes)
+    }
+    
+
 
         
 
