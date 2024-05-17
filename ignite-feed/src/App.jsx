16881b51fcf34f947bda3fa59a350efc06cf377c
Ciloe <gabiciloe@gmail.com>
2024-05-17 08:05:45 -0300

    Ignite feed
    Ignite feed

---
+++ ignite-feed/src/App.jsx
@@ -0,0 +1,13 @@
+import './global.css'
+import { Header } from '../components/Header'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
